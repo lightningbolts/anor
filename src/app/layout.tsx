a084bd0889eb478204d709064a2d68d813fddb2d
Kairui Cheng <timberlake2025@gmail.com>
2025-07-20 18:39:42 -0700

    Added @vercel/analytics dependency and integrate Analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Cinzel, Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 // Import fonts and set CSS variables
 const cinzel = Cinzel({
@@ -44,6 +45,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
